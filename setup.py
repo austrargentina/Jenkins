ed84db80cb477357beacc115429d94dca3d53f4a
austrargentina <dbracher@student.tgm.ac.at>
2014-11-15 19:09:24 +0100

    Changed setup.py so Import works

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ Created on 10.10.2014
 @contact: daniel.scheuch@student.tgm.ac.at
 @description: Starts our services
 '''
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'GUI'))
+
 from GUI.Controller import View
 if __name__ == '__main__':    
     v = View()
